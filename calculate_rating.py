02ef8438160eb1bf8ff179ef9cf329fbfca49bec
小浪 <138182057+mizuhara27@users.noreply.github.com>
2024-03-02 20:25:03 +0800

    Add files via upload

---
+++ calculate_rating.py
@@ -0,0 +1,49 @@
+import json
+json_path="musics.json"
+
+def load_music_data_from_json(json_file_path):
+    with open(json_file_path, 'r', encoding='utf-8') as f:
+        music_data = json.load(f)
+    return music_data
+
+def calculate_rating(constant, score):
+    #传入定数和分数计算单曲rating
+    if score >= 1009000:
+        return constant + 2.15
+    elif 1007500 <= score < 1009000:
+        return constant + 2.0 + 0.15 * (score - 1007500) / 1500
+    elif 1005000 <= score < 1007500:
+        return constant + 1.5 + 0.5 * (score - 1005000) / 2500
+    elif 1000000 <= score < 1005000:
+        return constant + 1.0 + 0.5 * (score - 1000000) / 5000
+    elif 975000 <= score < 1000000:
+        return constant + (score - 975000) / 25000
+    elif 925000 <= score < 975000:
+        return constant - 3.0 + 3.0 * (score - 925000) / 50000
+    elif 900000 <= score < 925000:
+        return constant - 5.0 + 2.0 * (score - 900000) / 25000
+    elif 800000 <= score < 900000:
+        return (constant - 5.0) / 2 + (constant - 5.0) / 2 * (score - 800000) / 100000
+    else:
+        return 0
+
+def get_difficulty_value(music_id, music_data):
+    music_id = str(music_id)
+    for music_entry in music_data:
+        if music_entry['id'] == music_id:
+            return music_entry['difficulties']
+    return None
+
+def convert_level_to_difficulty(level):
+    difficulty_mapping = {
+        0: "basic",
+        1: "advanced",
+        2: "expert",
+        3: "master",
+        4: "ultima",
+        5: "world's end"
+    }
+    return difficulty_mapping.get(level, None)
+
+
+
---
+++ chunib30.py
@@ -0,0 +1,58 @@
+import sqlite3
+from calculate_rating import calculate_rating,get_difficulty_value,load_music_data_from_json,convert_level_to_difficulty
+
+# 指定数据库文件和定数表的路径
+db_file_path = ''
+json_path="musics.json"
+# 连接到数据库文件
+conn = sqlite3.connect(db_file_path)
+
+# 创建一个游标对象，用于执行SQL语句
+cursor = conn.cursor()
+
+"""查询数据库并计算歌曲对应rating"""
+cursor.execute('SELECT music_id, score_max,level,is_all_justice,is_full_combo FROM chusan_user_music_detail')
+rows = cursor.fetchall()
+rating_list = []
+"""定数列表"""
+music_data = load_music_data_from_json(json_path)
+
+for row in rows:
+    music_id, score_max,level,is_all_justice,is_full_combo = row
+    """获取定数"""
+    difficulty_data=get_difficulty_value(music_id, music_data)
+    difficulte=convert_level_to_difficulty(level)
+    constant=difficulty_data.get(difficulte, None)
+    """获取单曲rating"""
+    rating_value = calculate_rating(constant, score_max)
+    rating_list.append({'music_id': music_id, 'score_max': score_max, 'rating': rating_value,'level':level,'constant':constant,'is_all_justice':is_all_justice,'is_full_combo':is_full_combo})
+    """取rating最高的30首乐曲"""
+sorted_rating_list = sorted(rating_list, key=lambda x: x['rating'], reverse=True)
+top_30_ratings = sorted_rating_list[:30]
+for item in top_30_ratings:
+    """将 'rating' 对应的值保留两位小数"""
+    item['rating'] = round(item['rating'], 2)
+count=1
+for rating_item in top_30_ratings:
+    music_id=rating_item['music_id']
+    music_id = str(music_id)
+    difficulte=convert_level_to_difficulty(rating_item['level'])
+    for music_entry in music_data:
+        if music_entry['id'] == music_id:
+            music_name=music_entry['name']
+    rating = str(round(rating_item['rating'], 2))  # 将评分转换为字符串并保留两位小数
+    score_max = str(rating_item['score_max'])
+    constant=str(rating_item['constant'])
+    idx=str(count)
+    """输出是否AJ或者FC"""
+    if(rating_item['is_all_justice']==1):
+        print(idx + " " + music_name + " " + difficulte + " rating: " + constant + "->" + rating + " 最高分数：" + score_max+" AJ"+"\n")
+    elif(rating_item['is_full_combo']==1):
+        print(idx + " " + music_name + " " + difficulte + " rating: " + constant + "->" + rating + " 最高分数：" + score_max+" FC"+"\n")
+    else:
+        print(idx + " " + music_name + " " + difficulte + " rating: " + constant + "->" + rating + " 最高分数：" + score_max+"\n")
+    count=count+1
+
+# 关闭游标和数据库连接
+cursor.close()
+conn.close()
